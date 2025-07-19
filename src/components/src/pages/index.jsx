b11b81488f1a0c0a8f544a003192f67ce98c4b3c
nldcrafter <nldcrafter1111@gmail.com>
2025-07-19 12:29:54 +0630

    Create index.jsx

---
+++ src/components/src/pages/index.jsx
@@ -0,0 +1,10 @@
+import AvodaWallet from "@/components/AvodaWallet";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Avoda</h1>
+      <AvodaWallet />
+    </div>
+  );
+}
